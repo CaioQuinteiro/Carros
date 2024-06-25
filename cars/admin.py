edb46167c59bc4b0c67aad3ada55be2e61c79151
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-06-24 22:24:32 -0300

    Alteracao no banco de dados

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from cars.models import Car, Brand
 
-# Register your models here.
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value',)
+    search_fields = ('model',)
+
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Car, CarAdmin)
