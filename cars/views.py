7f6275d68640384df333c9e8d4c34ffa1a71cd88
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-06-27 14:49:35 -0300

    Autalização Forms

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cars.models import Car
+from cars.forms import CarForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -13,3 +14,14 @@ def cars_view(request):
         'cars.html', 
         {'cars': cars }
     )
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:   
+        new_car_form = CarForm()
+    
+    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
