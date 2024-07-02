4b0492ebd771a0cfe86f1bef9fd64476e1efef7d
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-07-02 12:51:11 -0300

    Validações cars

--- cars/forms.py
+++ cars/forms.py
@@ -1,24 +1,19 @@
 from django import forms
 from cars.models import Brand, Car
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
 
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor minimo do carro deve ser de R$20.000,00')
+        return value
+    
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possivel cadastrar carros fabricados antes de 1975')
+        return factory_year
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -17,11 +17,11 @@ def cars_view(request):
 
 def new_car_view(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
+        new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
     else:   
-        new_car_form = CarForm()
+        new_car_form = CarModelForm()
     
     return render(request, 'new_car.html', { 'new_car_form': new_car_form })
