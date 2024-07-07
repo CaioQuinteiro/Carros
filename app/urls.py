5bbaf4fc859b8669b283bce42a82f1907fc36762
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-07-07 14:03:48 -0300

    Atualiação para Class based view

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from accounts.views import login_view, logout_view, register_view
-from cars.views import cars_view, new_car_view
+from cars.views import CarsView, NewCarView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('cars/', cars_view, name='cars_list'),
-    path('new_car/', new_car_view, name='new_car'),
+    path('cars/', CarsView.as_view(), name='cars_list'),
+    path('new_car/', NewCarView.as_view(), name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import redirect, render
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class CarsView(View):
+    
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
 
-    if search: 
-        cars = cars.filter(model__icontains=search)
+        if search: 
+             cars = cars.filter(model__icontains=search)
 
-    return render(
-        request, 
-        'cars.html', 
-        {'cars': cars }
-    )
+        return render(
+             request, 
+             'cars.html', 
+             {'cars': cars }
+        )
 
-def new_car_view(request):
-    if request.method == 'POST':
+class NewCarView(View):
+
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:   
-        new_car_form = CarModelForm()
-    
-    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+        return render(request, 'new_car.html', { 'new_car_form': new_car_form })
