b3bd3501ca3a4cedad1121642408cca98904b727
CaioQuinteiro <caioquinteiro7@gmail.com>
2024-07-09 20:01:34 -0300

    Atualizacao e finalização do CRUD

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView, CarDeleteView
+from accounts.views import register_view, login_view, logout_view
 
-from accounts.views import login_view, logout_view, register_view
-from cars.views import CarDetailView, CarsListView, NewCarCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +13,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
-    path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
+    path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
